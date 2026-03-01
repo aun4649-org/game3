fc808ddf85075eafcb2a9372a5e7cdfb1f54659f
aun4649 <165726804+aun4649-org@users.noreply.github.com>
2026-03-01 16:50:27 +0900

    Fixed

--- test_direct.js
+++ test_direct.js
@@ -2,37 +2,35 @@ const fs = require('fs');
 global.window = { cosmosInterruptFlag: false };
 eval(fs.readFileSync('./js/runtime.js', 'utf8'));
 
-async function test1() {
+async function testUnclosed() {
     const mem = new Uint8Array(65536);
     const progMem = {
-        10: '"WARAWA "',
+        10: '"WARAWA',
         20: '#=10'
     };
 
-    let printCount = 0;
-    global.runtimePrint = (s) => {
-        if (printCount < 5) {
-            console.log("PRINT:", s);
+    global.runtimePrint = console.log;
+    global.runtimePrintln = console.log;
+
+    // Safety break
+    let limit = 0;
+    const oldEvalPeek = CosmosRuntime.prototype.evalPeek;
+    CosmosRuntime.prototype.evalPeek = function () {
+        limit++;
+        if (limit > 500) {
+            console.log("INFINITE LOOP CAUGHT IN EVAL PEEK");
+            process.exit(1);
         }
-        printCount++;
-        if (printCount > 5) {
-            // Simulate interrupt
-            global.window.cosmosInterruptFlag = true;
-        }
-    };
-    global.runtimePrintln = (s) => {
-        if (printCount < 5) {
-            console.log("PRINTLN:", s);
-        }
+        return oldEvalPeek.call(this);
     };
 
     try {
         console.log("Running direct: #=1");
         await cosmosExecuteDirect("#=1", progMem, mem);
-        console.log("Finished direct execution");
+        console.log("Finished execution");
     } catch (e) {
         console.error("Caught error:", e);
     }
 }
 
-test1();
+testUnclosed();
